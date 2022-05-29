9ae86ccc0d8deab784b9cf052f005ede9c118085
lanzv <vojtech.lanz@gmail.com>
2022-05-29 17:04:30 +0200

    speedup geoportal process

--- utils/extractors.py
+++ utils/extractors.py
@@ -7,22 +7,21 @@ GML_POS_PATTERN = re.compile(r'\<gml:pos\>([0-9. ]*)\<\/gml:pos\>')
 def extractGMLCoordinates(gml_path: str):
     """
     """
-    coordinates = set()
+    coordinates = []
     gml_file = open(gml_path, "r")
     gml_content = gml_file.read()
     for posList in re.findall(GML_POSLIST_PATTERN, gml_content):
-        coordinates = coordinates.union(__parsePosList(posList))
+        __parsePosList(posList, coordinates)
     for pos in re.findall(GML_POS_PATTERN, gml_content):
-        coordinates.add(pos)
-    return coordinates
+        coordinates.append(pos)
+    return set(coordinates)
 
-def __parsePosList(posList: str):
+def __parsePosList(posList: str, coordinates: list):
     """
     """
     numbers = posList.split(' ')
-    coordinates = set()
     if len(numbers)%2 != 0:
         raise Exception("GML file coordinates are wrongly formated.")
     for i in range((int)(len(numbers)/2)):
-        coordinates.add(numbers[2*i] + " " + numbers[2*i + 1])
+        coordinates.append(numbers[2*i] + " " + numbers[2*i + 1])
     return coordinates
--- utils/output_generators.py
+++ utils/output_generators.py
@@ -6,26 +6,25 @@ from math import fabs
 def generate_jsonfile(name: str, coordinates: set, color = "red", radius = 1, intensity = 1):
     """
     """
-    final_json = ""
-    final_json += "{\n"
-    final_json += "\t\"name\": \"{}\",\n".format(name)
-    final_json += "\t\"color\": \"{}\",\n".format(color)
-    final_json += "\t\"data\": [\n"
+    final_json = open(name+".json", 'a')
+    final_json.write("{\n")
+    final_json.write("\t\"name\": \"{}\",\n".format(name))
+    final_json.write("\t\"color\": \"{}\",\n".format(color))
+    final_json.write("\t\"data\": [\n")
     first = True
     for coor in coordinates:
         if first:
             first = False
         else:
-            final_json += ",\n"
-        final_json += "\t\t{\n"
-        final_json += "\t\t\t\"long\": {},\n".format(coor.split(' ')[0])
-        final_json += "\t\t\t\"lat\": {},\n".format(coor.split(' ')[1])
-        final_json += "\t\t\t\"radius\": {},\n".format(radius)
-        final_json += "\t\t\t\"intensity\": {}\n".format(intensity)
-        final_json += "\t\t}"
-    final_json += "\n"
-    final_json += "\t]\n"
-    final_json += "}"
-    f = open(name+".json", "a")
-    f.write(final_json)
-    f.close()
+            final_json.write(",\n")
+        final_json.write("\t\t{\n")
+        final_json.write("\t\t\t\"long\": {},\n".format(coor.split(' ')[0]))
+        final_json.write("\t\t\t\"lat\": {},\n".format(coor.split(' ')[1]))
+        final_json.write("\t\t\t\"radius\": {},\n".format(radius))
+        final_json.write("\t\t\t\"intensity\": {}\n".format(intensity))
+        final_json.write("\t\t}")
+    final_json.write("\n")
+    final_json.write("\t]\n")
+    final_json.write("}")
+    
+    final_json.close()
